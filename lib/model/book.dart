1dc566dfeefe82013bd88b5fde88a09a5674cb41
halimeh-manaf <Halimeh-Manaf>
2021-04-06 19:54:46 +0200

    First DB init

--- lib/model/book.dart
+++ lib/model/book.dart
@@ -1,3 +1,5 @@
+import 'package:booktracer/database/columns.dart';
+
 class Book {
   String title;
   DateTime startDate;
@@ -5,6 +7,7 @@ class Book {
   int pageNumber;
   int totalPagesNumber;
   bool isDone;
+  int id = 0;
 
   Book(
       {this.title,
@@ -12,5 +15,33 @@ class Book {
       this.pageNumber,
       this.totalPagesNumber,
       this.isDone,
-      this.endDate});
+      this.endDate,
+      this.id});
+
+  // convenience constructor to create a Word object
+  Book.fromMap(Map<String, dynamic> map) {
+    id = map[columnId];
+    title = map[columnTitle];
+    pageNumber = map[columnPageNumber];
+    startDate = DateTime.fromMicrosecondsSinceEpoch(map[columnStartDate]);
+    endDate = DateTime.fromMicrosecondsSinceEpoch(map[columnEndDate]);
+    totalPagesNumber = map[columnTotalPageNumber];
+    isDone = map[columnIsDone];
+  }
+
+  // convenience method to create a Map from this Word object
+  Map<String, dynamic> toMap() {
+    var map = <String, dynamic>{
+      columnTitle: title,
+      columnPageNumber: pageNumber,
+      columnStartDate: startDate.microsecondsSinceEpoch,
+      columnEndDate: endDate.microsecondsSinceEpoch,
+      columnTotalPageNumber: totalPagesNumber,
+      columnIsDone: isDone
+    };
+    if (id != null) {
+      map[columnId] = id;
+    }
+    return map;
+  }
 }
