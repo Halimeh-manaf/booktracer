f8fe1ecee56c71cf47c632d37d4325a9436a0eb4
halimeh-manaf <halimeh.manaf@gmail.com>
2021-05-05 22:57:27 +0200

    code refactoring

--- lib/screens/book_screen.dart
+++ lib/screens/book_screen.dart
@@ -1,4 +1,5 @@
 import 'package:booktracer/model/book_provider.dart';
+import 'package:booktracer/widget/header_with_book_title.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -8,61 +9,31 @@ class BookScreen extends StatelessWidget {
   const BookScreen({this.id});
   @override
   Widget build(BuildContext context) {
-    Size size = MediaQuery.of(context).size;
+    Size Screensize = MediaQuery.of(context).size;
     return Scaffold(
       appBar: AppBar(
         title: Text(
             Provider.of<BookProvider>(context, listen: false).books[id].title),
       ),
-      body: Column(
-        children: [
-          Container(
-            height: size.height * 0.15,
-            child: Stack(
-              children: [
-                Container(
-                  height: size.height * 0.15 - 27,
-                  decoration: BoxDecoration(
-                    color: Colors.blueAccent,
-                    borderRadius: BorderRadius.only(
-                        bottomLeft: Radius.circular(36),
-                        bottomRight: Radius.circular(36.0)),
-                  ),
-                ),
-                Positioned(
-                  bottom: 0,
-                  right: 0,
-                  left: 0,
-                  child: Container(
-                    margin: EdgeInsets.symmetric(horizontal: 20.0),
-                    padding: EdgeInsets.symmetric(horizontal: 20.0),
-                    height: 54,
-                    decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(20.0),
-                        boxShadow: [
-                          BoxShadow(
-                              offset: Offset(0, 10),
-                              blurRadius: 50.0,
-                              color: Colors.black.withOpacity(0.23))
-                        ]),
-                    child: Center(
-                        child: Text(
-                      Provider.of<BookProvider>(context, listen: false)
-                          .books[id]
-                          .title,
-                      style: TextStyle(fontSize: 17.0),
-                    )),
-                  ),
-                ),
-              ],
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            HeaderWithBookTitle(size: Screensize, id: id),
+            Container(
+              margin: EdgeInsets.symmetric(vertical: 20.0, horizontal: 5.0),
+              padding: EdgeInsets.symmetric(vertical: 20.0),
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(20.0),
+                  boxShadow: [
+                    BoxShadow(
+                        offset: Offset(0, 10),
+                        blurRadius: 50.0,
+                        color: Colors.black.withOpacity(0.23))
+                  ]),
             ),
-          ),
-          Container(
-            color: Colors.black,
-            height: 30,
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
